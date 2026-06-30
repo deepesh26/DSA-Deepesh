a038d23bd4670c23872719bb65a4d5c4d04fcaa9
deepesh26 <144758261+deepesh26@users.noreply.github.com>
2026-06-30 20:51:53 +0000

    restructure Basic match concept

---
+++ TakeYouForward/TUF-DSA/BasicMathConcepts/ArmStrongNum.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ArmStrongNum {
+
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        int orginal = n;
+        int sum = 0;
+        while(n>0){
+            int digit = n%10;
+            n = n/10;
+            sum = sum+(digit*digit*digit);
+        }
+        if(sum==orginal){
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
+        
+    }
+}
